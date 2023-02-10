94f9a99342911a33d6c2609f6f5002907ca17cea
BertieWoo88 <050688V@gmail.com>
2023-02-10 14:07:54 +0300

    Task3 added

---
+++ Task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine()!);
+int chislo = 2; 
+while(chislo<=N) 
+{
+    Console.Write(" " + chislo);
+    chislo+=2; 
+}
